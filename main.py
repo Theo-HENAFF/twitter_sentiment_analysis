7c830f5a4eeedd44add69ecfa8811c5cafff7e80
HENAFF <theo.henaff@hotmail.fr>
2020-10-21 15:01:34 +0300

    add one word cleaning to csv_cleaning.py add viz to main.py and some results

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from sklearn.model_selection import train_test_split
 
 import tensorflow as tf
 
+import matplotlib.pyplot as plt
+
 # ------------------------------------------------------
 # Setup GPU
 # ------------------------------------------------------
@@ -36,7 +38,7 @@ tf.keras.callbacks.EarlyStopping(
 # ------------------------------------------------------
 data = pd.read_csv(
     # "C:/Users/Théo/Documents/twitter_sentiment_analysis/data/cleaned_data.csv",
-    "C:/Users/HENAFF/Documents/Cours Polytech/S9 en Roumanie/Machine Learning - ML/data/mid_cleaned_data.csv",
+    "C:/Users/HENAFF/Documents/Cours Polytech/S9 en Roumanie/Machine Learning - ML/data/small_cleaned_data.csv",
     # nrows=20000,
     encoding='latin-1')
 data['clean_text'] = data.clean_text.astype(str)
@@ -52,7 +54,7 @@ X_pad = pad_sequences(X_seq, maxlen=30, padding='post')  #maxlen must be equal t
 
 X_train, X_test, y_train, y_test = train_test_split(X_pad, data['polarity'].values, test_size=0.25, random_state=1, shuffle= True)
 
-batch_size = 500
+batch_size = 128
 X_train1 = X_train[batch_size:]
 y_train1 = y_train[batch_size:]
 X_valid = X_train[:batch_size]
@@ -71,9 +73,9 @@ embedding_size = 32
 model = Sequential()
 model.add(Embedding(vocabulary_size, embedding_size, input_length=max_words))
 
-model.add(Bidirectional(LSTM(500, return_sequences=True)))
-model.add(Bidirectional(LSTM(500, return_sequences=True)))
-model.add(Bidirectional(LSTM(500)))
+# model.add(Bidirectional(LSTM(32, return_sequences=True)))
+# model.add(Bidirectional(LSTM(32, return_sequences=True)))
+model.add(Bidirectional(LSTM(256)))
 
 # model.add(LSTM(200, return_sequences=True))
 # model.add(LSTM(200, return_sequences=True))
@@ -82,10 +84,44 @@ model.add(Bidirectional(LSTM(500)))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(X_train1, y_train1, shuffle=True, validation_data=(X_valid, y_valid), batch_size=batch_size, epochs=20)
+history = model.fit(X_train1, y_train1, shuffle=True, validation_data=(X_valid, y_valid), batch_size=batch_size, epochs=5)
 
 # ------------------------------------------------------
 # Evaluate the model performance
 # ------------------------------------------------------
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Test accuracy : ", scores[1])
+
+# # Get training and test loss histories
+# training_loss = history.history['loss']
+# test_loss = history.history['val_loss']
+#
+# # Create count of the number of epochs
+# epoch_count = range(1, len(training_loss) + 1)
+#
+# # Visualize loss history
+# plt.plot(epoch_count, training_loss, 'r--')
+# plt.plot(epoch_count, test_loss, 'b-')
+# plt.legend(['Training Loss', 'Test Loss'])
+# plt.xlabel('Epoch')
+# plt.ylabel('Loss')
+# plt.show();
+
+
+loss = history.history['loss']
+loss_val = history.history['val_loss']
+accuracy = history.history['accuracy']
+accuracy_val = history.history['val_accuracy']
+epochs = range(1,len(loss)+1)
+
+plt.plot(epochs, loss, 'b', label='Training loss')
+plt.plot(epochs, loss_val, 'b--', label='validation loss')
+plt.plot(epochs, accuracy, 'r', label='Training accuracy')
+plt.plot(epochs, accuracy_val, 'r--', label='validation accuracy')
+
+plt.title('Training and Validation loss')
+plt.xlabel('Epochs')
+plt.legend()
+plt.grid()
+plt.show()
+
